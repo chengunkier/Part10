4cc44a2587218ebc5d64efbcd41c3c2e54c7c505
chengunkier <inabamicheal97@gmail.com>
2026-09-22 11:54:37 +0300

    completed exercise 11. Fetching repositories with Apollo Client

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
+import { ApolloProvider } from '@apollo/client/react';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
+
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
     <>
-      <StatusBar style="auto" />
-      <NativeRouter>
-        <Main />
+      <StatusBar style="light" />
+      <NativeRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
     </>
   );
