af3a32dd011aa9bb1b83a1ad591a8d46f43ddc3e
chengunkier <inabamicheal97@gmail.com>
2026-09-23 16:32:45 +0300

    completed exercise The sign in form mutation

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 import theme from '../theme';
+import useSignIn from '../hooks/useSignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -37,8 +38,17 @@ const validationSchema = yup.object().shape({
 });
 
 const SignIn = () => {
-  const onSubmit = (values) => {
-    console.log(values);
+  const [signIn] = useSignIn();
+
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const formik = useFormik({
